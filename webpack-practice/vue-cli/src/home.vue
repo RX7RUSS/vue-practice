bdbb23c6e54baca7c16f1ea369323f7e51f1907b
RX7RUSS <russ.savage5@gmail.com>
2018-01-31 22:54:28 -0600

    Props Project Added

---
+++ webpack-practice/vue-cli/src/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>First Component: {{ greeting }}</p>
+    <hr>
+    <button @click="changeStatus">Change Greeting</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        greeting: 'Hello World'
+      }
+    },
+    methods: {
+      changeStatus() {
+        this.greeting = 'Goodbye';
+      }
+    }
+  }
+</script>
--- webpack-practice/vue-cli/src/main.js
+++ webpack-practice/vue-cli/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import home from './home';
+
+Vue.component('greeting', home);
 
 new Vue({
   el: '#app',
