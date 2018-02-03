c9b96aa3fb9b033082da86d282515b2180f7c7a0
RX7RUSS <russ.savage5@gmail.com>
2018-02-02 22:19:27 -0600

    Event Bus

--- user-props/src/components/UserDetail.vue
+++ user-props/src/components/UserDetail.vue
@@ -3,13 +3,22 @@
         <h3>You may view the User Details here</h3>
         <p>User Details</p>
         <p>User Name: {{ switchName() }}</p>
+        <p>User Age: {{ userAge }}</p>
         <button @click="resetName">Reset User</button>
     </div>
 </template>
 
 <script>
+  import { eventBus } from '../main';
+
   export default {
-    props: ['name'],
+    props: {
+      name: {
+        type: String
+      },
+      resetFn: Function,
+      userAge: Number
+    },
     methods: {
       switchName() {
         return this.name;
@@ -18,6 +27,11 @@
         this.name = 'Russ';
         this.$emit('nameWasReset', this.name);
       }
+    },
+    created() {
+      eventBus.$on('ageWasEdited', (age) => {
+        this.userAge = age;
+      });
     }
   }
 </script>
--- user-props/src/components/UserEdit.vue
+++ user-props/src/components/UserEdit.vue
@@ -2,10 +2,24 @@
     <div class="component">
         <h3>You may edit the User here</h3>
         <p>Edit me!</p>
+        <button @click="editAge">Edit Age</button>
+        <p>User Age: {{ userAge }}</p>
     </div>
 </template>
 
 <script>
+import { eventBus } from '../main';
+
+export default {
+  props: ['userAge'],
+  methods: {
+    editAge() {
+      this.userAge = 30;
+
+      eventBus.changeAge(this.userAge);
+    }
+  }
+}
 </script>
 
 <style scoped>
