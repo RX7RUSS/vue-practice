6b7ee6b15cc9019af232d5988a0b36768016116b
RX7RUSS <russ.savage5@gmail.com>
2018-02-11 22:04:56 -0600

    Create Event Bus

--- quotes/src/main.js
+++ quotes/src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+export const eventBus = new Vue({
+  methods: {
+    newQuote(quote) {
+      this.$emit('quoteWasAdded', quote);
+    }
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
