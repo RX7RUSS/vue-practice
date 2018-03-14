bb8153913e8173b56e3a83627ea6938f9597c2ab
RX7RUSS <russ.savage5@gmail.com>
2018-03-14 17:29:27 -0500

    Testing QR Component

---
+++ qr-reader-test/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/e/e5/NASA_logo.svg">
+    <Search/>
+  </div>
+</template>
+
+<script>
+import Search from './components/Search'
+
+export default {
+  name: 'App',
+  components: {
+    Search
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 80px;
+}
+</style>
