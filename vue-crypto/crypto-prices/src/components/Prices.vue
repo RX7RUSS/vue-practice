87c0d22f9924ea5ba155e57bd9090fac46f7f13c
RX7RUSS <russ.savage5@gmail.com>
2018-05-07 10:28:54 -0500

    Simple Crypto Price App

---
+++ vue-crypto/crypto-prices/src/components/Prices.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+    <div class="title">Current Crypto Prices in USD</div>
+    <div id="crypto-container" v-for="(value, key) in cryptos" :key="key">
+      <span class="left">{{ key }}</span>
+      <span class="right">${{ value.USD }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Prices',
+  data: () => ({
+    cryptos: [],
+    errors: []
+  }),
+
+  created () {
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT,BCH,NANO,BNB,XLM,XRP&tsyms=USD')
+      .then(response => {
+        this.cryptos = response.data
+        console.log(response)
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+  body {
+    background: #f1f1f1;
+  }
+
+  div#crypto-container {
+    background: white;
+    width: 70%;
+    margin: 0 auto 4px auto;
+    padding: 1rem;
+    box-shadow: 1px 1px 0 lightgrey;
+  }
+
+  .title {
+    text-align: center;
+    font-size: 1.4rem;
+    font-weight: 800;
+    background: white;
+    width: 70%;
+    margin: 0 auto 4px auto;
+    padding: 1rem;
+    box-shadow: 1px 1px 0 lightgrey;
+  }
+
+  span.left {
+    font-weight: bold;
+  }
+
+  span.right {
+    float: right;
+  }
+
+</style>
